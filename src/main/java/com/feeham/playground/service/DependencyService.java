2bdc8401b4e84a465df53a3687be1012e82df3ac
feehaam <mdfeeham@gmail.com>
2024-02-11 22:38:16 +0600

    Dependency serivce updated

--- src/main/java/com/feeham/playground/service/DependencyService.java
+++ src/main/java/com/feeham/playground/service/DependencyService.java
@@ -3,18 +3,22 @@ package com.feeham.playground.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.time.Duration;
 
 @Service
 public class DependencyService {
 
+    private final WebClient webClient;
     public final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public DependencyService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("https://dummyjson.com").build();
+    }
 
     public Mono<?> oneDependent(){
         System.out.println("Got into dependent call method.");
-        var response = externalService()
+        var response = externalService(1)
                 .doOnSubscribe(subscription -> {
                     System.out.println("Subscribed to external service.");
                 })
@@ -26,20 +30,30 @@ public class DependencyService {
     }
 
     public Mono<?> twoIndependent(){
-        var response1 = externalService();
-        var response2 = externalService();
+        var response1 = externalService(1);
+        var response2 = externalService(2);
         return Mono.just("External calls executed.");
     }
 
     public Mono<?> oneIndependentOneDependent(){
-        var response1 = externalService();
-        var response2 = externalService();
+        var response1 = externalService(1);
+        var response2 = externalService(2);
         return response1;
     }
 
-    private Mono<String> externalService(){
-        System.out.println("External service executed.");
-        return Mono.just("External data.")
-                .delayElement(Duration.ofSeconds(1));
+    private Mono<Object> externalService(Integer productId){
+        System.out.println("Fetching product: " + productId + ". " + Thread.currentThread().getName());
+        long time = System.currentTimeMillis();
+        return webClient.get()
+                .uri("/products/{productId}", productId)
+                .retrieve()
+                .bodyToMono(Object.class)
+                .flatMap(result -> {
+                    System.out.println(result);
+                    System.out.println("Started at " + time + " ended at " + System.currentTimeMillis()
+                            + " | Diff: " + (System.currentTimeMillis() - time)
+                            + " --> Thread: " + Thread.currentThread().getName());
+                    return Mono.just(result);
+                });
     }
 }
